e0cdc2a4048e26b1000c7716497cd05b1c92d4ac
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-06-27 23:38:09 +0530

    Urls for password-reset

--- mart/urls.py
+++ mart/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 import shop.views as shop_view
@@ -39,7 +41,7 @@ urlpatterns = [
     path('seller-activation/', shop_view.seller_activation),
     path('seller-login/', shop_view.seller_login),
     path('add-product-category/', shop_view.add_product_category),
-
+    url('^', include('django.contrib.auth.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
